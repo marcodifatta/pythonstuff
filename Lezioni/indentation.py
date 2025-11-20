9a886220af61ed24a6ae509205e4bbc079094e96
Droghyz <m.di.fatta08@gmail.com>
2025-11-20 22:13:48 +0100

    Esercizi

---
+++ Lezioni/indentation.py
@@ -0,0 +1,15 @@
+#Usiamo gli spazi per creare il blocco di codice
+if 5 > 2:
+  print("Five is greater than two!")
+
+#Il numero di spazi è indifferente, ma deve essere coerente, per convenzione si usano 4 spazi(2 volte tab), ma anche 2 vanno bene(1 volta tab).
+
+if 5 > 2:
+    print("Five is greater than two!")
+
+#Dobbiamo usare sempre gli stessi spazi nello stesso blocco di codice altrimenti python darà un indentation error.
+
+# Le variabili in python si assegnano normalmente.
+
+x = 3
+y = "Hello world"
---
+++ Lezioni/version.py
@@ -0,0 +1,4 @@
+# Per verificare la versione di Python usiamo sys
+import sys
+
+print(sys.version)
